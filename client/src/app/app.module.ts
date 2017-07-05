17b8de0f005fed78b4e6bbbf237b9cae12b493c5
Chris <chriskim80@gmail.com>
2017-07-05 10:18:33 -0400

    Saving changes

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,23 +1,27 @@
 import { environment } from './../environments/environment';
-import { LoginPage } from './../pages/login/login';
-import { SignupPage } from './../pages/signup/signup';
-import { ResetPasswordPage } from './../pages/reset-password/reset-password';
 import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { Geolocation } from '@ionic-native/geolocation';
-
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireAuthModule } from 'angularfire2/auth';
-import { AuthProvider } from '../providers/auth/auth';
 import { HttpModule } from '@angular/http';
+
+//Pages
+import { LoginPage } from './../pages/login/login';
+import { SignupPage } from './../pages/signup/signup';
+import { ResetPasswordPage } from './../pages/reset-password/reset-password';
 import { TabsPage } from './../pages/tabs/tabs';
 import { MapPage } from './../pages/map/map';
 import { DeliveryPage } from './../pages/delivery/delivery';
+
+//Providers
+import { AuthProvider } from '../providers/auth/auth';
 import { UserProvider } from '../providers/user/user';
+import { Geolocation } from '@ionic-native/geolocation';
+
 
 @NgModule({
   declarations: [
