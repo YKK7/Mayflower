6dc55f2259ea9d1bf2e3afd37806c1eadf05d470
Chris <chriskim80@gmail.com>
2017-07-05 11:51:12 -0400

    Fixed some spacing

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -10,36 +10,36 @@ import { LoginPage } from '../pages/login/login';
 
 
 @Component({
-  template: `<ion-nav [root]="rootPage"></ion-nav>`
+    template: `<ion-nav [root]="rootPage"></ion-nav>`
 })
 export class MyApp {
-  @ViewChild(Nav) nav: Nav;
+    @ViewChild(Nav) nav: Nav;
 
-  rootPage: any;
-  loader: any;
+    rootPage: any;
+    loader: any;
 
-  pages: Array<{ title: string, component: any }>;
+    pages: Array<{ title: string, component: any }>;
 
-  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen, public afAuth: AngularFireAuth,
-    public loadingCtrl: LoadingController) {
-    this.initializeApp();
-    const authObserver = afAuth.authState.subscribe(user => {
-      if (user) {
-        this.rootPage = TabsPage;
-        authObserver.unsubscribe();
-      } else {
-        this.rootPage = LoginPage;
-        authObserver.unsubscribe();
-      }
-    });
+    constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen, public afAuth: AngularFireAuth,
+        public loadingCtrl: LoadingController) {
+        this.initializeApp();
+        const authObserver = afAuth.authState.subscribe(user => {
+            if (user) {
+                this.rootPage = TabsPage;
+                authObserver.unsubscribe();
+            } else {
+                this.rootPage = LoginPage;
+                authObserver.unsubscribe();
+            }
+        });
 
-  }
+    }
 
-  initializeApp() {
-    this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
-    });
-  }
+    initializeApp() {
+        this.platform.ready().then(() => {
+            this.statusBar.styleDefault();
+            this.splashScreen.hide();
+        });
+    }
 }
 
--- client/src/pages/map/map.ts
+++ client/src/pages/map/map.ts
@@ -3,95 +3,92 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Geolocation } from '@ionic-native/geolocation';
 import { UserProvider } from '../../providers/user/user';
 
-
-// declare var google;
-
 @IonicPage()
 @Component({
-  selector: 'page-map',
-  templateUrl: 'map.html',
+    selector: 'page-map',
+    templateUrl: 'map.html',
 })
 export class MapPage {
 
-  @ViewChild('map') mapElement: ElementRef;
-  @ViewChild('directionsPanel') directionPanel: ElementRef;
-  map: any;
-  labels: any;
-  labelIndex: any;
-  private users: Array<any>;
+    @ViewChild('map') mapElement: ElementRef;
+    @ViewChild('directionsPanel') directionPanel: ElementRef;
+    map: any;
+    labels: any;
+    labelIndex: any;
+    private users: Array<any>;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public geoLocation: Geolocation, public userProvider: UserProvider) {
-  }
+    constructor(public navCtrl: NavController, public navParams: NavParams, public geoLocation: Geolocation, public userProvider: UserProvider) {
+    }
 
-  ionViewDidLoad() {
-    this.loadMap();
-    this.userProvider.getUsers().subscribe(users => {
-      this.users = users;
-    })
-  }
+    ionViewDidLoad() {
+        this.loadMap();
+        this.userProvider.getUsers().subscribe(users => {
+            this.users = users;
+        })
+    }
 
-  showDrivers() {
-    for (const user of this.users) {
-      if (user.type === 2) {
-        let firstName = user.name.substring(0, user.name.indexOf(' '));
-        let userLatLng = { lat: user.latitude, lng: user.longitude };
-        let marker = new google.maps.Marker({
-          map: this.map,
-          label: firstName,
-          position: userLatLng
+    showDrivers() {
+        for (const user of this.users) {
+            if (user.type === 2) {
+                let firstName = user.name.substring(0, user.name.indexOf(' '));
+                let userLatLng = { lat: user.latitude, lng: user.longitude };
+                let marker = new google.maps.Marker({
+                    map: this.map,
+                    label: firstName,
+                    position: userLatLng
+                });
+                let content = "<h4>" + user.name + "</h4>";
+                this.addInfoWindow(marker, content);
+            }
+        }
+        this.map.panTo(this.driverCenter());
+        this.map.setZoom(12);
+    }
+
+    driverCenter(): google.maps.LatLng {
+        let latAvg = 0;
+        let longAvg = 0;
+        let count = 0;
+        for (const user of this.users) {
+            if (user.type === 2) {
+                count++;
+                latAvg += user.latitude;
+                longAvg += user.longitude;
+            }
+        }
+
+        latAvg /= count;
+        longAvg /= count;
+        return new google.maps.LatLng(latAvg, longAvg);
+    }
+
+    loadMap() {
+        navigator.geolocation.getCurrentPosition(position => {
+
+            let latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+            const mapOptions = {
+                center: latlng,
+                zoom: 15,
+                mapTypeId: google.maps.MapTypeId.ROADMAP,
+                streetViewControl: false
+            };
+
+            this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
+        }, (err) => console.error(err));
+    }
+
+    addInfoWindow(marker, content) {
+
+        let infoWindow = new google.maps.InfoWindow({
+            content: content
         });
-        let content = "<h4>" + user.name + "</h4>";
-        this.addInfoWindow(marker, content);
-      }
+
+        google.maps.event.addListener(marker, 'click', () => {
+            infoWindow.open(this.map, marker);
+        });
+
     }
-    this.map.panTo(this.driverCenter());
-    this.map.setZoom(12);
-  }
-
-  driverCenter(): google.maps.LatLng {
-    let latAvg = 0;
-    let longAvg = 0;
-    let count = 0;
-    for (const user of this.users) {
-      if (user.type === 2) {
-        count++;
-        latAvg += user.latitude;
-        longAvg += user.longitude;
-      }
-    }
-
-    latAvg /= count;
-    longAvg /= count;
-    return new google.maps.LatLng(latAvg, longAvg);
-  }
-
-  loadMap() {
-    navigator.geolocation.getCurrentPosition(position => {
-
-      let latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-
-      const mapOptions = {
-        center: latlng,
-        zoom: 15,
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-        streetViewControl: false
-      };
-
-      this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
-
-    }, (err) => console.error(err));
-  }
-
-  addInfoWindow(marker, content) {
-
-    let infoWindow = new google.maps.InfoWindow({
-      content: content
-    });
-
-    google.maps.event.addListener(marker, 'click', () => {
-      infoWindow.open(this.map, marker);
-    });
-
-  }
 
 }
